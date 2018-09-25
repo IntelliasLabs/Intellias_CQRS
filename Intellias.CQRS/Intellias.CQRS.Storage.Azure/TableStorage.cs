2b3cecb6fd9e9c789f92ba379d6a1dda2765dcbc
Sergey Seletsky <SeletskySergey@live.com>
2018-09-25 14:26:58 +0300

    added dummy storage

---
+++ Intellias.CQRS/Intellias.CQRS.Storage.Azure/TableStorage.cs
@@ -0,0 +1,132 @@
+﻿using Intellias.CQRS.Core.Messages;
+using Intellias.CQRS.Core.Storage;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intellias.CQRS.Storage.Azure
+{
+    public class TableStorage<T> : IStorage<T> where T : BaseEntity, new()
+    {
+        private readonly CloudTableClient client;
+        private readonly CloudTable table;
+
+        public TableStorage(StorageCredentials creds)
+        {
+            var account = new CloudStorageAccount(creds, true);
+            client = account.CreateCloudTableClient();
+            table = client.GetTableReference(typeof(T).Name);
+
+            // Create the CloudTable if it does not exist
+            table.CreateIfNotExistsAsync().Wait();
+        }
+
+        private T GetResult(TableResult response)
+        {
+            if (response == null || response.Result == null)
+            {
+                return null;
+            }
+            return (response.Result as StorageEntity).GetValue<T>();
+        }
+
+        public async Task<T> CreateAsync(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            if (string.IsNullOrWhiteSpace(entity.Id))
+            {
+                entity.Id = Unified.NewCode();
+            }
+
+            var op = TableOperation.Insert(new StorageEntity(entity));
+
+            // Execute the insert operation.
+            var response = await table.ExecuteAsync(op);
+
+            return GetResult(response);
+        }
+
+        public async Task<T> DeleteAsync(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+
+            var entity = await OneAsync(id.ToUpperInvariant());
+
+            var op = TableOperation.Delete(new StorageEntity(entity));
+
+            var response = await table.ExecuteAsync(op);
+
+            return GetResult(response);
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public async Task<T> OneAsync(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+
+            var op = TableOperation.Retrieve<StorageEntity>(id.ToUpperInvariant().First().ToString(), id.ToUpperInvariant());
+
+            // Execute the retrieve operation.
+            var response = await table.ExecuteAsync(op);
+
+            return GetResult(response);
+        }
+
+        public async Task<IQueryable<T>> QueryAsync(Expression<Func<T, bool>> predicate = null)
+        {
+            // Construct the query operation for all customer entities where PartitionKey="Smith".
+            var query = new TableQuery<StorageEntity>();
+            // query.Where() use predicate here
+            TableContinuationToken token = null;
+            var items = new List<T>();
+
+            do
+            {
+                var segment = await table.ExecuteQuerySegmentedAsync(query, token);
+                token = segment.ContinuationToken;
+
+                items.AddRange(segment.Results.Select(x => x.GetValue<T>()));
+            } while (token != null);
+
+            return items.AsQueryable();
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            if (string.IsNullOrWhiteSpace(entity.Id))
+            {
+                throw new ArgumentNullException(nameof(entity.Id));
+            }
+
+            // Execute the insert operation.
+            var response = await table.ExecuteAsync(TableOperation.Replace(new StorageEntity(entity)));
+
+            return GetResult(response);
+        }
+    }
+}
