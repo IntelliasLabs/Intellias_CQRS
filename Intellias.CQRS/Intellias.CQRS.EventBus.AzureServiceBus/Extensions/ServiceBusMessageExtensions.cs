2ac05e808418a28bfe3204babb029767a0395c9c
Andrii Sokalskyi <asokalskyi@intellias.com>
2018-09-27 21:01:18 +0300

    Added Azure Service Bus as Event Bus

---
+++ Intellias.CQRS/Intellias.CQRS.EventBus.AzureServiceBus/Extensions/ServiceBusMessageExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using Intellias.CQRS.Core.Events;
+using Intellias.CQRS.Core.Messages;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+
+namespace Intellias.CQRS.EventBus.AzureServiceBus.Extensions
+{
+    internal static class ServiceBusMessageExtensions
+    {
+        public static Message ToBusMessage(this IEvent @event)=>
+            new Message(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(@event)))
+            {
+                MessageId = Unified.NewCode(),
+                PartitionKey = @event.AggregateRootId
+            };
+    }
+}
