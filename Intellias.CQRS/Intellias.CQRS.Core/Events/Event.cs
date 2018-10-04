d4ddcb40cd92897272e2cdb4eeb8ac1bdc81ecd6
Sergey Seletsky <SeletskySergey@live.com>
2018-10-04 13:48:40 +0300

    fixed version to id set

--- Intellias.CQRS/Intellias.CQRS.Core/Events/Event.cs
+++ Intellias.CQRS/Intellias.CQRS.Core/Events/Event.cs
@@ -6,17 +6,32 @@ namespace Intellias.CQRS.Core.Events
     /// <inheritdoc cref="IEvent" />
     public abstract class Event : AbstractMessage, IEvent
     {
+        private int version;
+
         /// <inheritdoc />
         protected Event()
         {
-            Id = Unified.NewCode(Version);
+        }
+
+        /// <inheritdoc />
+        protected Event(int version)
+        {
+            Version = version;
         }
 
         /// <inheritdoc />
         public string AggregateRootId { get; set; }
 
         /// <inheritdoc />
-        public int Version { get; set; }
+        public int Version
+        {
+            get => version;
+            set
+            {
+                version = value;
+                Id = Unified.NewCode(Version);
+            }
+        }
 
         /// <inheritdoc />
         /// <summary>
--- Intellias.CQRS/Intellias.CQRS.Core/Messages/AbstractMessage.cs
+++ Intellias.CQRS/Intellias.CQRS.Core/Messages/AbstractMessage.cs
@@ -13,5 +13,77 @@
 
         /// <inheritdoc />
         public string Id { get; protected set; }
+
+        /// <inheritdoc />
+        public override string ToString() => Id;
+
+        /// <summary>
+        /// Base equality operator
+        /// </summary>
+        /// <param name="x">This</param>
+        /// <param name="y">Other</param>
+        /// <returns>Is equal</returns>
+        public static bool operator ==(AbstractMessage x, AbstractMessage y)
+        {
+            return Equals(x, y);
+        }
+
+        /// <summary>
+        /// Base inversed equality operator
+        /// </summary>
+        /// <param name="x">This</param>
+        /// <param name="y">Other</param>
+        /// <returns>Is equal</returns>
+        public static bool operator !=(AbstractMessage x, AbstractMessage y)
+        {
+            return !(x == y);
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as AbstractMessage);
+        }
+
+        private static bool IsTransient(AbstractMessage obj)
+        {
+            return obj != null && string.IsNullOrWhiteSpace(obj.Id);
+        }
+
+        /// <summary>
+        /// Base equality operator
+        /// </summary>
+        /// <param name="other">Other</param>
+        /// <returns>Is equal</returns>
+        protected virtual bool Equals(AbstractMessage other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            if (!IsTransient(this) &&
+                !IsTransient(other) &&
+                Equals(Id, other.Id))
+            {
+                var otherType = other.GetType();
+                var thisType = GetType();
+                return thisType.IsAssignableFrom(otherType) ||
+                        otherType.IsAssignableFrom(thisType);
+            }
+
+            return false;
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return Unified.Decode(Id).GetHashCode();
+        }
     }
 }
