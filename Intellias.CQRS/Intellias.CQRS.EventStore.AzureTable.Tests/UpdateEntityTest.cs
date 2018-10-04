7c1e8ce43b2ec2d5cc719f2e9e36dbae9feb9f81
Andrii Sokalskyi <asokalskyi@intellias.com>
2018-10-04 18:24:54 +0300

    Analyzer fix

--- Intellias.CQRS/Intellias.CQRS.EventStore.AzureTable.Tests/UpdateEntityTest.cs
+++ Intellias.CQRS/Intellias.CQRS.EventStore.AzureTable.Tests/UpdateEntityTest.cs
@@ -3,10 +3,17 @@ using Intellias.CQRS.EventStore.AzureTable.Tests.Core;
 
 namespace Intellias.CQRS.EventStore.AzureTable.Tests
 {
-    internal class UpdateEntityTest : BaseTest
+    /// <summary>
+    /// UpdateEntityTest
+    /// </summary>
+    public class UpdateEntityTest : BaseTest
     {
         private readonly string testId = Unified.NewCode();
         private readonly string testData = "Test Data";
+
+        /// <summary>
+        /// UpdateEntityTest
+        /// </summary>
         public UpdateEntityTest()
         {
             CreateItem(testId, testData);
