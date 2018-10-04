aa276f147c13b8c72f7d6459990e902ff8f28aab
Andrii Sokalskyi <asokalskyi@intellias.com>
2018-10-04 11:24:59 +0300

    Test start

---
+++ Intellias.CQRS/Intellias.CQRS.EventStore.AzureTable.Tests/Core/TestEntity.cs
@@ -0,0 +1,26 @@
+﻿using Intellias.CQRS.Core.Domain;
+
+namespace Intellias.CQRS.EventStore.AzureTable.Tests.Core
+{
+    /// <summary>
+    /// Test aggregate root
+    /// </summary>
+    public class TestEntity : AggregateRoot
+    {
+        /// <summary>
+        /// Do not allow to create objects without Id
+        /// </summary>
+        protected TestEntity() { }
+
+        /// <summary>
+        /// Main constructor of the class
+        /// </summary>
+        /// <param name="id"></param>
+        public TestEntity(string id) : base(id)
+        {
+            Handles<TestCreatedEvent>(OnTestCreated);
+            Handles<TestUpdatedEvent>(OnTestUpdated);
+            Handles<TestDeletedEvent>(OnTestDeleted);
+        }
+    }
+}
---
+++ Intellias.CQRS/Intellias.CQRS.EventStore.AzureTable.Tests/Core/TestUpdatedEvent.cs
@@ -0,0 +1,22 @@
+﻿using Intellias.CQRS.Core.Events;
+
+namespace Intellias.CQRS.EventStore.AzureTable.Tests.Core
+{
+    /// <inheritdoc />
+    public class TestUpdatedEvent : Event
+    {
+        /// <summary>
+        /// TestUpdateEvent
+        /// </summary>
+        /// <param name="aggregateRootId"></param>
+        public TestUpdatedEvent(string aggregateRootId)
+        {
+            AggregateRootId = aggregateRootId;
+        }
+
+        /// <summary>
+        /// TestData
+        /// </summary>
+        public string TestData { get; set; }
+    }
+}
