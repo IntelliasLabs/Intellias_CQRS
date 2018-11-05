36bad9b6575a71ca25c91df26f74dfdeddafbb19
Sergey Seletsky <SeletskySergey@live.com>
2018-11-05 17:25:01 +0200

    added azure command bus

---
+++ Intellias.CQRS/Intellias.CQRS.CommandBus.Azure.Tests/AzureCommandBusTests.cs
@@ -0,0 +1,41 @@
+using Intellias.CQRS.Core.Messages;
+using Intellias.CQRS.Tests.Core.Commands;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Xunit;
+
+namespace Intellias.CQRS.CommandBus.Azure.Tests
+{
+    /// <summary>
+    /// Azure Command Bus Integration Tests
+    /// </summary>
+    public class AzureCommandBusTests
+    {
+        /// <summary>
+        /// Test publishing command to azure table and queue
+        /// </summary>
+        [Fact]
+        public void PublishCommandTest()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
+
+            var storeConnectionString = configuration.GetConnectionString("TableStorageConnection");
+            var account = CloudStorageAccount.Parse(storeConnectionString);
+
+            var commandBus = new AzureCommandBus(account);
+
+            var cmd = new TestUpdateCommand
+            {
+                AggregateRootId = "Competency.1",
+                ExpectedVersion = 1,
+                TestData = "test data string",
+                UserId = "test@user.com"
+            };
+            cmd.Metadata.Add(MetadataKey.AgreegateType, "Competency");
+            var result = commandBus.PublishAsync(cmd).Result;
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ Intellias.CQRS/Intellias.CQRS.CommandBus.Azure/CommandTableEntity.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Intellias.CQRS.Core.Commands;
+using Intellias.CQRS.Core.Messages;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
+
+namespace Intellias.CQRS.CommandBus.Azure
+{
+    /// <summary>
+    /// Command Table Entity
+    /// </summary>
+    public class CommandTableEntity : TableEntity, ICommand
+    {
+        private readonly ICommand cmd;
+
+        /// <inheritdoc />
+        public CommandTableEntity(ICommand cmd, string content)
+        {
+            this.cmd = cmd;
+            PartitionKey = cmd.AggregateRootId;
+            RowKey = $"{cmd.ExpectedVersion}.{cmd.Id}";
+            Data = content;
+            ETag = "*";
+        }
+
+        /// <summary>
+        /// Holds the entity data
+        /// </summary>
+        public string Data { set; get; }
+
+        /// <inheritdoc />
+        public string Id => cmd.Id;
+
+        /// <inheritdoc />
+        public int ExpectedVersion { get => cmd.ExpectedVersion; set => throw new NotSupportedException(); }
+
+        /// <inheritdoc />
+        public string AggregateRootId { get => cmd.AggregateRootId; set => throw new NotSupportedException(); }
+
+        /// <inheritdoc />
+        public string UserId { get => cmd.UserId; set => throw new NotSupportedException(); }
+
+        /// <inheritdoc />
+        public DateTime Created => cmd.Created;
+
+        /// <inheritdoc />
+        public IDictionary<MetadataKey, string> Metadata => cmd.Metadata;
+
+        /// <summary>
+        /// Returns deserialized data object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public T GetValue<T>() =>
+            string.IsNullOrEmpty(Data)
+                ? default(T)
+                : JsonConvert.DeserializeObject<T>(Data);
+    }
+}
