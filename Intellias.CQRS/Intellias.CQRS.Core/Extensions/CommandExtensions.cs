895fa7fcfc0e357bae9d3900b3201155328ae901
Dmytro Mazur <dmazur@intellias.com>
2018-12-06 14:12:09 +0000

    Merged PR 116: ToSimpleDomainEvent command extension is added
    ToSimpleDomainEvent command extension is added
    
    Related work items: #261

---
+++ Intellias.CQRS/Intellias.CQRS.Core/Extensions/CommandExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Intellias.CQRS.Core.Commands;
+using Intellias.CQRS.Core.Events;
+using Intellias.CQRS.Core.Messages;
+
+namespace Intellias.CQRS.Core.Extensions
+{
+    /// <summary>
+    /// Extension for commands
+    /// </summary>
+    public static class CommandExtensions
+    {
+        /// <summary>
+        /// Converts common command data to event
+        /// </summary>
+        /// <typeparam name="E">event without specific properties</typeparam>
+        /// <param name="c">command</param>
+        /// <returns></returns>
+        public static E ToSimpleDomainEvent<E>(this Command c)
+            where E : Event, new()
+        {
+            var @event = new E
+            {
+                AggregateRootId = c.AggregateRootId,
+                Version = c.ExpectedVersion
+            };
+
+            @event.CopyMetadataFrom(c);
+
+            return @event;
+        }
+    }
+}
