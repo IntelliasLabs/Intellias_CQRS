c0b77623c8717bf2af8ffccd7b5f99efd0ecc060
Sergey Seletsky <SeletskySergey@live.com>
2019-02-22 22:51:39 +0200

    marged tests

---
+++ Intellias.CQRS/Intellias.CQRS.Tests/Core/BaseTest.cs
@@ -0,0 +1,108 @@
+using System.Collections.Generic;
+using Intellias.CQRS.Core.Config;
+using Intellias.CQRS.Core.Events;
+using Intellias.CQRS.Tests.Core.Commands;
+using Intellias.CQRS.Tests.Core.Domain;
+using Intellias.CQRS.Tests.Core.Events;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Moq;
+using Newtonsoft.Json;
+
+namespace Intellias.CQRS.EventStore.AzureTable.Tests.Core
+{
+    /// <summary>
+    /// Keeps all init data
+    /// </summary>
+    public class BaseTest
+    {
+        /// <summary>
+        /// EventStore
+        /// </summary>
+        protected IEventStore Store { get; }
+
+        /// <summary>
+        /// EventBus
+        /// </summary>
+        protected Mock<IEventBus> BusMock { get; }
+
+        /// <summary>
+        /// aggregateTable
+        /// </summary>
+        protected CloudTable AggregateTable { get; }
+
+        /// <summary>
+        /// eventTable
+        /// </summary>
+        protected CloudTable EventTable { get; }
+
+        /// <summary>
+        /// Base constructor
+        /// </summary>
+        public BaseTest()
+        {
+            JsonConvert.DefaultSettings = CqrsSettings.JsonConfig;
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
+
+            var storeConnectionString = configuration.GetConnectionString("TableStorageConnection");
+
+            BusMock = new Mock<IEventBus>();
+
+            Store = new AzureTableEventStore(storeConnectionString, BusMock.Object);
+
+            var tableClient = CloudStorageAccount
+                .Parse(storeConnectionString)
+                .CreateCloudTableClient();
+
+            AggregateTable = tableClient.GetTableReference("AggregateStore");
+            EventTable = tableClient.GetTableReference("EventStore");
+        }
+
+        /// <summary>
+        /// CreateItem
+        /// </summary>
+        /// <param name="id"></param>
+        /// /// <param name="testData"></param>
+        protected void CreateItem(string id, string testData)
+        {
+            var item = new TestRoot(new TestCreateCommand
+            {
+                AggregateRootId = id,
+                TestData = testData
+            });
+            Store.SaveAsync(item).Wait();
+        }
+
+        /// <summary>
+        /// UpdateItem
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="testData"></param>
+        protected void UpdateItem(string id, string testData)
+        {
+            var item = new TestRoot(id);
+
+            // Generating virtual load of events..
+            item.LoadFromHistory(new List<IEvent>
+            {
+                new TestCreatedEvent
+                {
+                    AggregateRootId = id,
+                    Version = 1,
+                    TestData = "Data to be updated...."
+                }
+            });
+
+            item.Update(new TestUpdateCommand
+            {
+                AggregateRootId = id,
+                TestData = testData
+            });
+            Store.SaveAsync(item).Wait();
+        }
+    }
+}
---
+++ Intellias.CQRS/Intellias.CQRS.Tests/CreateEntityTests.cs
@@ -0,0 +1,68 @@
+using System.Linq;
+using Intellias.CQRS.Core.Events;
+using Intellias.CQRS.Core.Messages;
+using Intellias.CQRS.EventStore.AzureTable.Documents;
+using Intellias.CQRS.EventStore.AzureTable.Tests.Core;
+using Intellias.CQRS.Tests.Core.Domain;
+using Microsoft.WindowsAzure.Storage.Table;
+using Moq;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Intellias.CQRS.EventStore.AzureTable.Tests
+{
+    /// <summary>
+    /// CreateEntityTests
+    /// </summary>
+    public class WhenEntityCreated : BaseTest
+    {
+        private readonly string testId = Unified.NewCode();
+        private readonly string testData = "Test Data";
+
+        /// <summary>
+        /// WhenEntityCreated
+        /// </summary>
+        public WhenEntityCreated()
+        {
+            CreateItem(testId, testData);
+        }
+
+        /// <summary>
+        /// Publish method called once
+        /// </summary>
+        [Fact]
+        public void ShouldCallOnceServiceBusPublishMethod()
+        {
+            BusMock.Verify(x=>x.PublishAsync(It.IsAny<IEvent>()), Times.Once);
+        }
+
+        /// <summary>
+        /// Check is AggregateRoot record present
+        /// </summary>
+        [Fact]
+        public void ShouldCreateAggregateRootRecordWithVersion1()
+        {
+            var operation = TableOperation.Retrieve<EventStoreAggregate>(typeof(TestRoot).Name, testId);
+            var result = (EventStoreAggregate)AggregateTable.ExecuteAsync(operation).Result.Result;
+
+            Assert.True(result.LastArVersion == 1, "Test version for aggregated root is not equal 1");
+        }
+
+        /// <summary>
+        /// Check if event serialized 
+        /// </summary>
+        [Fact]
+        public void ShouldCreateEventRecordWithVersion1AndTestData()
+        {
+            var query = new TableQuery<EventStoreEvent>()
+                .Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, testId));
+            var result = EventTable.ExecuteQuerySegmentedAsync(query, null).Result.Results;
+
+            var record = result.First().Data;
+            dynamic @event = JsonConvert.DeserializeObject(record);
+
+            Assert.True(@event.Version == 1, "Test version for created event is not equal 1");
+            Assert.True(@event.TestData == testData, "Test data for created event is lost");
+        }
+    }
+}
