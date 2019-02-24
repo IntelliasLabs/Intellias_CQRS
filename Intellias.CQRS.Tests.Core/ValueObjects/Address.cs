2954f196d74402271d0893a63261d0d065020964
Sergey Seletsky <SeletskySergey@live.com>
2019-02-24 17:31:04 +0200

    added value object test

---
+++ Intellias.CQRS.Tests.Core/ValueObjects/Address.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Intellias.CQRS.Core.Domain;
+
+namespace Intellias.CQRS.Tests.Core.ValueObjects
+{
+    /// <summary>
+    /// Address value object
+    /// </summary>
+    public class Address : ValueObject<Address>
+    {
+        /// <summary>
+        /// Address Line
+        /// </summary>
+        public string AddressLine { set; get; } = string.Empty;
+
+        /// <summary>
+        /// Post Code
+        /// </summary>
+        public string PostCode { set; get; }
+
+        /// <inheritdoc />
+        protected override bool EqualsCore(Address other) => string.Equals(this.AddressLine, other.AddressLine, StringComparison.InvariantCulture);
+
+        /// <inheritdoc />
+        protected override int GetHashCodeCore()
+        {
+            return AddressLine.GetHashCode();
+        }
+    }
+}
---
+++ Intellias.CQRS.Tests/ValueObjectTests.cs
@@ -0,0 +1,27 @@
+﻿using Intellias.CQRS.Tests.Core.ValueObjects;
+using Xunit;
+
+namespace Intellias.CQRS.Tests.Core
+{
+    public class ValueObjectTests : BaseTest
+    {
+        [Fact]
+        public void ShouldBeAvailableInGetById()
+        {
+            var address1 = new Address {
+                AddressLine = "test1"
+            };
+
+            var address2 = new Address
+            {
+                AddressLine = "test2"
+            };
+
+            Assert.NotEqual(address1, address2);
+
+            address2.AddressLine = address1.AddressLine;
+
+            Assert.Equal(address1, address2);
+        }
+    }
+}
