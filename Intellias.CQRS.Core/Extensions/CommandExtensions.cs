46436d80602555c1289c5a01cd8c9bd53e418cb8
Andrii Sokalskyi <asokalskyi@intellias.com>
2019-03-28 14:45:49 +0000

    Merged PR 489: Removed process-manager logic and fixed command-to-event logic
    Removed process-manager logic and fixed command-to-event logic (added missed Id generation)
    
    Related work items: #17

--- Intellias.CQRS.Core/Extensions/CommandExtensions.cs
+++ Intellias.CQRS.Core/Extensions/CommandExtensions.cs
@@ -14,38 +14,17 @@ namespace Intellias.CQRS.Core.Extensions
         /// Converts common command data to event
         /// </summary>
         /// <typeparam name="TEvent">event without specific properties</typeparam>
-        /// <param name="c">command</param>
+        /// <param name="command">command</param>
         /// <returns></returns>
-        public static TEvent ToSimpleDomainEvent<TEvent>(this Command c)
+        public static TEvent ToEvent<TEvent>(this Command command)
             where TEvent : Event, new()
         {
-            var @event = new TEvent
-            {
-                AggregateRootId = c.AggregateRootId,
-                Version = c.ExpectedVersion,
-                SourceId = c.Id,
-                CorrelationId = c.CorrelationId
-            };
 
-            @event.CopyMetadataFrom(c);
+            var @event = command.ToType<TEvent>();
+            @event.SourceId = command.Id;
+            @event.Version = command.ExpectedVersion;
 
             return @event;
-        }
-
-        /// <summary>
-        /// Converts some ProcessCommand type to another
-        /// </summary>
-        /// <typeparam name="TCommand"></typeparam>
-        /// <param name="command"></param>
-        /// <returns></returns>
-        public static TCommand ToType<TCommand>(this ProcessCommand command)
-            where TCommand: ProcessCommand, new()
-        {
-            var result = ((AbstractMessage)command).ToType<TCommand>();
-            result.ExpectedVersion = command.ExpectedVersion;
-            result.Process = command.Process;
-
-            return result;
         }
 
         /// <summary>
