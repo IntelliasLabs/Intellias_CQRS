bf4d2f59aee349f301397a69b57aff49a900fa3e
Sergey Seletsky <SeletskySergey@live.com>
2019-05-13 20:31:22 +0300

    added test

---
+++ Intellias.CQRS.Tests/AzureReportBusClientTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Intellias.CQRS.EventBus.AzureServiceBus;
+using Microsoft.Azure.ServiceBus;
+using Moq;
+using Xunit;
+
+namespace Intellias.CQRS.Tests
+{
+    public class AzureReportBusClientTest
+    {
+        [Fact]
+        public void SubscribeTest()
+        {
+            var mock = new Mock<ISubscriptionClient>();
+
+            var reportBus = new AzureReportBusClient(mock.Object);
+            reportBus.Subscribe(e => {
+
+                // Is event received
+                Assert.NotNull(e);
+                return Task.FromResult(e);
+            });
+
+            //Todo mock pushing an event
+        }
+    }
+}
