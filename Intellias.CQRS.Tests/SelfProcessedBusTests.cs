2d14fb26f8c059444216a0c96b4c066cd64a8fcd
Sergey Seletsky <SeletskySergey@live.com>
2019-05-14 16:26:51 +0300

    added tests

---
+++ Intellias.CQRS.Tests/SelfProcessedBusTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Intellias.CQRS.Core.Tools;
+using Intellias.CQRS.Tests.Core.Commands;
+using Intellias.CQRS.Tests.Core.Events;
+using Intellias.CQRS.Tests.Core.Fakes;
+using Moq;
+using Xunit;
+
+namespace Intellias.CQRS.Tests
+{
+    public class SelfProcessedBusTests
+    {
+        [Fact]
+        public void SelfCommandBusTest()
+        {
+            var sp = new Mock<IServiceProvider>();
+            var bus = new SelfProcessedCommandBus(new HandlerManager(new HandlerDependencyResolver(sp.Object, new List<Assembly>())));
+
+            var result = bus.PublishAsync(new TestCreateCommand()).Result;
+            Assert.True(result.IsSuccess);
+        }
+
+        [Fact]
+        public void SelfEventBusTest()
+        {
+            var sp = new Mock<IServiceProvider>();
+            var bus = new SelfProcessedEventBus(new HandlerManager(new HandlerDependencyResolver(sp.Object, new List<Assembly>())));
+
+            var result = bus.PublishAsync(new TestCreatedEvent()).Result;
+            Assert.True(result.IsSuccess);
+        }
+    }
+}
