103ca229d0fafe327fbae42af4d528cd6fe989e6
Sergey Seletsky <SeletskySergey@live.com>
2019-07-09 21:27:43 +0300

    fix

--- Intellias.CQRS.Tests/Messages/ExecutionResultTest.cs
+++ Intellias.CQRS.Tests/Messages/ExecutionResultTest.cs
@@ -13,16 +13,16 @@ namespace Intellias.CQRS.Tests.Messages
         public void SerializeTest()
         {
             var result = ExecutionResult.Failed("Test error", new Exception("Test exception"));
-            result.Error.AddError(new ExecutionError("Name", "Test field error"));
+            result.Error?.AddError(new ExecutionError("Name", "Test field error"));
 
             var json = JsonConvert.SerializeObject(result, CqrsSettings.JsonConfig());
 
             var deserialized = JsonConvert.DeserializeObject<ExecutionResult>(json, CqrsSettings.JsonConfig());
 
-            Assert.Equal(result.Error.ErrorMessage, deserialized.Error.ErrorMessage);
-            Assert.Equal(result.Error.Exception.Message, deserialized.Error.Exception.Message);
+            Assert.Equal(result.Error?.ErrorMessage, deserialized.Error?.ErrorMessage);
+            Assert.Equal(result.Error?.Exception?.Message, deserialized.Error?.Exception?.Message);
             Assert.Equal(result.Success, deserialized.Success);
-            Assert.Equal(result.Error.Errors.First().ErrorMessage, deserialized.Error.Errors.First().ErrorMessage);
+            Assert.Equal(result.Error?.Errors.First().ErrorMessage, deserialized.Error?.Errors.First().ErrorMessage);
         }
     }
 }
