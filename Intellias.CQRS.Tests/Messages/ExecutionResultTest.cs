c8ad4a9287ebe19dd749c7fee5a810858640e347
Sergey Seletsky <SeletskySergey@live.com>
2019-07-12 14:35:55 +0300

    fix

--- Intellias.CQRS.Tests/Messages/ExecutionResultTest.cs
+++ Intellias.CQRS.Tests/Messages/ExecutionResultTest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using Intellias.CQRS.Core;
 using Intellias.CQRS.Core.Results;
+using Intellias.CQRS.Tests.Core.Domain;
 using Xunit;
 
 namespace Intellias.CQRS.Tests.Messages
@@ -21,6 +22,16 @@ namespace Intellias.CQRS.Tests.Messages
             Assert.Equal(result.ErrorMessage, deserialized.ErrorMessage);
             Assert.Equal(result.Success, deserialized.Success);
             Assert.Equal(result.Errors.First().ErrorMessage, deserialized.Errors.First().ErrorMessage);
+        }
+
+        [Fact]
+        public void AggregateTest()
+        {
+            var ar = new TestRoot("code");
+            Assert.Equal(ErrorCodes.AccessDenied, ((FailedResult)ar.AccessDenied("Test")).ErrorCode);
+            Assert.Equal(ErrorCodes.ValidationFailed, ((FailedResult)ar.ValidationFailed("Test")).ErrorCode);
+            Assert.Equal(ErrorCodes.UnhandledError, ((FailedResult)ar.UnhandledError("Test")).ErrorCode);
+            Assert.Equal(ErrorCodes.VersionConflict, ((FailedResult)ar.Failed(ErrorCodes.VersionConflict, "Test")).ErrorCode);
         }
     }
 }
