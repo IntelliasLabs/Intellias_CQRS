39845907967fe8176b7e3fd434acbdc2c096c900
Dmytro Mazur <dmazur@intellias.com>
2019-08-09 16:40:08 +0300

    Added command attributes to mark if command fields needed some specific validation

---
+++ Intellias.CQRS.Core/Commands/Attributes/CoEIdAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Intellias.CQRS.Core.Commands.Attributes
+{
+    /// <summary>
+    /// Custom metadata marker that Center of Excelence should be verified
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class CoEIdAttribute : Attribute
+    {
+    }
+}
---
+++ Intellias.CQRS.Core/Commands/Attributes/UserIdAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Intellias.CQRS.Core.Commands.Attributes
+{
+    /// <summary>
+    /// Custom metadata marker that user id should be verified
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class UserIdAttribute : Attribute
+    {
+    }
+}
