f1d20f1229c274c18e0e51424fab3168163034a3
Pavlo Rytikov <prytikov@intellias.com>
2019-08-21 13:29:21 +0300

    Implement immutable query model store, add tests, refactor existing

--- Intellias.CQRS.QueryStore.AzureTable/Immutable/ImmutableQueryModelTableStorage.cs
+++ Intellias.CQRS.QueryStore.AzureTable/Immutable/ImmutableQueryModelTableStorage.cs
@@ -1,5 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Threading.Tasks;
 using Intellias.CQRS.Core.Queries.Immutable;
+using Intellias.CQRS.QueryStore.AzureTable.Options;
+using Microsoft.Extensions.Options;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
 
 namespace Intellias.CQRS.QueryStore.AzureTable.Immutable
 {
@@ -12,16 +21,112 @@ namespace Intellias.CQRS.QueryStore.AzureTable.Immutable
         IImmutableQueryModelWriter<TQueryModel>
         where TQueryModel : IImmutableQueryModel, new()
     {
-        /// <inheritdoc />
-        public Task<TQueryModel> FindAsync(string id, int version) => throw new System.NotImplementedException();
+        private readonly IOptionsMonitor<TableStorageOptions> options;
+        private readonly Lazy<CloudTable> tableClient;
+
+        public ImmutableQueryModelTableStorage(IOptionsMonitor<TableStorageOptions> options)
+        {
+            this.options = options;
+            tableClient = new Lazy<CloudTable>(CreateTable);
+        }
 
         /// <inheritdoc />
-        public Task<TQueryModel> GetAsync(string id, int version) => throw new System.NotImplementedException();
+        public async Task<TQueryModel> FindAsync(string id, int version)
+        {
+            var operation = TableOperation.Retrieve<ImmutableTableEntity>(id, GetRowKey(version));
+            var result = await tableClient.Value.ExecuteAsync(operation);
+            var entity = (ImmutableTableEntity)result.Result;
+
+            return entity == null ? default : entity.DeserializeQueryModel();
+        }
 
         /// <inheritdoc />
-        public Task<TQueryModel> GetLatestAsync(string id) => throw new System.NotImplementedException();
+        public async Task<TQueryModel> GetAsync(string id, int version)
+        {
+            var queryModel = await FindAsync(id, version);
+            if (queryModel == null)
+            {
+                throw new KeyNotFoundException($"No query model '{typeof(TQueryModel)}' with id '{id}' and version '{version}' is found.");
+            }
+
+            return queryModel;
+        }
 
         /// <inheritdoc />
-        public Task<TQueryModel> CreateAsync(TQueryModel model) => throw new System.NotImplementedException();
+        public async Task<TQueryModel> GetLatestAsync(string id)
+        {
+            var filter = TableQuery.GenerateFilterCondition(nameof(TableEntity.PartitionKey), QueryComparisons.Equal, id);
+            var query = new TableQuery<ImmutableTableEntity>()
+                .Where(filter)
+                .Take(1);
+
+            var querySegment = await tableClient.Value.ExecuteQuerySegmentedAsync(query, null);
+            var queryModel = querySegment.Results.Select(entity => entity.DeserializeQueryModel()).FirstOrDefault();
+
+            return queryModel;
+        }
+
+        /// <inheritdoc />
+        public async Task<TQueryModel> CreateAsync(TQueryModel model)
+        {
+            var entity = new ImmutableTableEntity(model);
+            var operation = TableOperation.Insert(entity);
+
+            var result = await tableClient.Value.ExecuteAsync(operation);
+
+            return ((ImmutableTableEntity)result.Result).DeserializeQueryModel();
+        }
+
+        private static string GetRowKey(int version)
+        {
+            // Build from Created row key that stores data in reverse order.
+            return string.Format(CultureInfo.InvariantCulture, "{0:D20}", int.MaxValue - version);
+        }
+
+        private CloudTable CreateTable()
+        {
+            var client = CloudStorageAccount
+                .Parse(options.CurrentValue.ConnectionString)
+                .CreateCloudTableClient();
+
+            var tableName = this.options.CurrentValue.TableNamePrefix + typeof(TQueryModel).Name;
+            var tableReference = client.GetTableReference(tableName);
+            if (!tableReference.ExistsAsync().GetAwaiter().GetResult())
+            {
+                tableReference.CreateIfNotExistsAsync().GetAwaiter().GetResult();
+            }
+
+            return tableReference;
+        }
+
+        private class ImmutableTableEntity : TableEntity
+        {
+            public ImmutableTableEntity()
+            {
+            }
+
+            public ImmutableTableEntity(TQueryModel queryModel)
+            {
+                PartitionKey = queryModel.Id;
+                RowKey = GetRowKey(queryModel.Version);
+                JsonQueryModel = JsonConvert.SerializeObject(queryModel);
+            }
+
+            public string JsonQueryModel { get; set; }
+
+            public TQueryModel DeserializeQueryModel()
+            {
+                if (string.IsNullOrWhiteSpace(JsonQueryModel))
+                {
+                    return default;
+                }
+
+                var queryModel = JsonConvert.DeserializeObject<TQueryModel>(JsonQueryModel);
+
+                queryModel.Timestamp = Timestamp;
+
+                return queryModel;
+            }
+        }
     }
 }
---
+++ Intellias.CQRS.Tests/QueryStores/ImmutableQueryModelTableStorageTests.cs
@@ -0,0 +1,96 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Intellias.CQRS.Core.Messages;
+using Intellias.CQRS.Core.Queries.Immutable;
+using Intellias.CQRS.QueryStore.AzureTable.Immutable;
+using Intellias.CQRS.QueryStore.AzureTable.Options;
+using Intellias.CQRS.Tests.Fakes;
+using Intellias.CQRS.Tests.Utils;
+using Intellias.CQRS.Tests.Utils.Fixtures;
+using Microsoft.WindowsAzure.Storage;
+using Xunit;
+
+namespace Intellias.CQRS.Tests.QueryStores
+{
+    public class ImmutableQueryModelTableStorageTests : StorageAccountTestBase
+    {
+        private readonly ImmutableQueryModelTableStorage<ImmutableQueryModel> storage;
+
+        public ImmutableQueryModelTableStorageTests(StorageAccountFixture fixture)
+            : base(fixture)
+        {
+            var options = new TableStorageOptions
+            {
+                TableNamePrefix = fixture.ExecutionContext.GetUniqueStorageTablePrefix(),
+                ConnectionString = fixture.Configuration.StorageAccount.ConnectionString
+            };
+
+            storage = new ImmutableQueryModelTableStorage<ImmutableQueryModel>(new OptionsMonitorFake<TableStorageOptions>(options));
+        }
+
+        [Fact]
+        public async Task Get_NoQueryModel_Throws()
+        {
+            await storage.Awaiting(s => s.GetAsync(Unified.NewCode(), 1)).Should().ThrowAsync<KeyNotFoundException>();
+        }
+
+        [Fact]
+        public async Task Find_NoQueryModel_ReturnsDefault()
+        {
+            (await storage.FindAsync(Unified.NewCode(), 1)).Should().BeNull();
+        }
+
+        [Fact]
+        public async Task Create_NoQueryModel_Creates()
+        {
+            var queryModel = new ImmutableQueryModel();
+
+            var stored = await storage.CreateAsync(queryModel);
+
+            stored.Should().BeEquivalentTo(queryModel, options => options.ForImmutableQueryModel());
+        }
+
+        [Fact]
+        public async Task Create_QueryModelAlreadyExist_Throws()
+        {
+            var queryModel = new ImmutableQueryModel();
+
+            await storage.CreateAsync(queryModel);
+
+            storage.Awaiting(s => s.CreateAsync(queryModel)).Should().Throw<StorageException>()
+                .Which.RequestInformation.HttpStatusCode.Should().Be((int)HttpStatusCode.Conflict);
+        }
+
+        [Fact]
+        public async Task GetLatestAsync_NoQueryModel_ReturnsDefault()
+        {
+            (await storage.GetLatestAsync(Unified.NewCode())).Should().BeNull();
+        }
+
+        [Fact]
+        public async Task GetLatestAsync_HasQueryModels_ReturnsLatest()
+        {
+            var id = Unified.NewCode();
+            await storage.CreateAsync(new ImmutableQueryModel { Id = id, Version = 1 });
+            var qm2 = await storage.CreateAsync(new ImmutableQueryModel { Id = id, Version = 2 });
+
+            var latest = await storage.GetLatestAsync(id);
+
+            latest.Should().BeEquivalentTo(qm2);
+        }
+
+        private class ImmutableQueryModel : BaseImmutableQueryModel
+        {
+            public ImmutableQueryModel()
+            {
+                Id = Unified.NewCode();
+                Version = 1;
+                SomeProperty = Unified.NewCode();
+            }
+
+            public string SomeProperty { get; set; }
+        }
+    }
+}
