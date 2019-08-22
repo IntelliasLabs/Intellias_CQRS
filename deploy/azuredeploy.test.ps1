61f53560f2ede984207dfb10940afd032b308ab2
Pavlo Rytikov <prytikov@intellias.com>
2019-08-22 11:43:50 +0300

    Add test output

--- deploy/azuredeploy.test.ps1
+++ deploy/azuredeploy.test.ps1
@@ -2,4 +2,6 @@ param ([Parameter(Mandatory=$true)][string]$armOutput)
 
 $json = $armOutput | ConvertFrom-Json
 
+Write-Host $json
+
 Write-Host "##vso[task.setvariable variable=StorageAccount.ConnectionString;]${json.testsStorageAccountConnectionString.value}"
