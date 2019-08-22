3b9b9610df29740e4c96c850fa23763c6668e33d
Pavlo Rytikov <prytikov@intellias.com>
2019-08-22 14:34:55 +0300

    Remove empty interface

--- Intellias.CQRS.Core/Events/IntegrationEvents/IIntegrationEvent.cs
+++
@@ -1,9 +0,0 @@
-namespace Intellias.CQRS.Core.Events.IntegrationEvents
-{
-    /// <summary>
-    /// Base interface for Integration Events.
-    /// </summary>
-    public interface IIntegrationEvent
-    {
-    }
-}
