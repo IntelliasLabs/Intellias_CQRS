818a28ebdd1898a76411d6afb7c38cc2796a3bc2
Pavlo Rytikov <prytikov@intellias.com>
2019-08-22 14:16:10 +0300

    Fix variable name

--- deploy/azuredeploy.test.ps1
+++ deploy/azuredeploy.test.ps1
@@ -2,4 +2,4 @@ param ([Parameter(Mandatory=$true)][string]$armOutput)
 
 $json = $armOutput | ConvertFrom-Json
 
-Write-Host ("##vso[task.setvariable variable=StorageAccount.ConnectionString;]{0}" -f $json.testsStorageAccountConnectionString.value)
+Write-Host ("##vso[task.setvariable variable=StorageAccount__ConnectionString;]{0}" -f $json.testsStorageAccountConnectionString.value)
