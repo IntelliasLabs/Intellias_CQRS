3236a5e008f5c7cf0cfdfd2471609f2f9e71c6e8
Pavlo Rytikov <prytikov@intellias.com>
2019-09-20 16:48:06 +0300

    Add missing implementations

--- src/Intellias.CQRS.Pipelines/IMessageDispatcher.cs
+++ src/Intellias.CQRS.Pipelines/IMessageDispatcher.cs
@@ -8,6 +8,13 @@ namespace Intellias.CQRS.Pipelines
     public interface IMessageDispatcher
     {
         /// <summary>
+        /// Dispatches command to a handlers.
+        /// </summary>
+        /// <param name="message">Serialized message.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        Task DispatchCommandAsync(string message);
+
+        /// <summary>
         /// Dispatches event to a handler.
         /// </summary>
         /// <param name="message">Serialized message.</param>
--- src/Intellias.CQRS.Pipelines/ServiceCollectionExtensions.cs
+++ src/Intellias.CQRS.Pipelines/ServiceCollectionExtensions.cs
@@ -1,4 +1,9 @@
+using System.Linq;
 using System.Reflection;
+using Intellias.CQRS.Core.Commands;
+using Intellias.CQRS.Core.Results;
+using Intellias.CQRS.Pipelines.CommandHandlers;
+using Intellias.CQRS.Pipelines.CommandHandlers.Behaviors;
 using Intellias.CQRS.Pipelines.EventHandlers;
 using Intellias.CQRS.Pipelines.EventHandlers.Notifications;
 using MediatR;
@@ -11,6 +16,58 @@ namespace Intellias.CQRS.Pipelines
     /// </summary>
     public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        /// Registers pipeline abstractions.
+        /// </summary>
+        /// <param name="services">Services collection.</param>
+        /// <param name="commandHandlersAssembly">Assembly where to look for command handlers.</param>
+        /// <param name="commandsAssembly">Assembly where to look for commands.</param>
+        /// <returns>Service collection with registered types.</returns>
+        public static IServiceCollection AddCommandHandlers(
+            this IServiceCollection services,
+            Assembly commandHandlersAssembly,
+            Assembly commandsAssembly)
+        {
+            return AddCommandHandlers(services, new[] { commandHandlersAssembly }, new[] { commandsAssembly });
+        }
+
+        /// <summary>
+        /// Registers pipeline abstractions.
+        /// </summary>
+        /// <param name="services">Services collection.</param>
+        /// <param name="commandHandlersAssemblies">Assemblies where to look for command handlers.</param>
+        /// <param name="commandsAssemblies">Assemblies where to look for commands.</param>
+        /// <returns>Service collection with registered types.</returns>
+        public static IServiceCollection AddCommandHandlers(
+            this IServiceCollection services,
+            Assembly[] commandHandlersAssemblies,
+            Assembly[] commandsAssemblies)
+        {
+            services.AddMediatR(commandHandlersAssemblies)
+                .AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>))
+                .AddTransient<IMessageDispatcher, MessageDispatcher>();
+
+            var subdomainCommandsType = commandsAssemblies.SelectMany(a => a.GetTypes())
+                .Where(t => t.GetInterfaces().Contains(typeof(ICommand)))
+                .ToArray();
+
+            foreach (var commandType in subdomainCommandsType)
+            {
+                var commandRequestType = typeof(CommandRequest<>).MakeGenericType(commandType);
+
+                var behaviorInterfaceType = typeof(IPipelineBehavior<,>).MakeGenericType(commandRequestType, typeof(IExecutionResult));
+                var publisherType = typeof(PublisherBehavior<>).MakeGenericType(commandType);
+                var validatorType = typeof(ValidationBehavior<>).MakeGenericType(commandType);
+                var transactionManagerType = typeof(TransactionManagerBehavior<>).MakeGenericType(commandType);
+
+                services.AddTransient(behaviorInterfaceType, publisherType);
+                services.AddTransient(behaviorInterfaceType, validatorType);
+                services.AddTransient(behaviorInterfaceType, transactionManagerType);
+            }
+
+            return services;
+        }
+
         /// <summary>
         /// Registers Integration event pipeline abstractions.
         /// </summary>
