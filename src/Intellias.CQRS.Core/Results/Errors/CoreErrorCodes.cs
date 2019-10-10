7f6796e105e2b15f21df503e5b0cb1692be25458
Dmytro Mazur <dmazur@intellias.com>
2019-10-10 12:16:47 +0000

    Merged PR 1443: Added 'ErrorCodeInfo' and 'CoreErrorCodes' classes
    Added 'ErrorCodeInfo' and 'CoreErrorCodes' classes
    
    Related work items: #1271

---
+++ src/Intellias.CQRS.Core/Results/Errors/CoreErrorCodes.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Intellias.CQRS.Core.Results.Errors
+{
+    /// <summary>
+    /// Common error codes collection.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public static class CoreErrorCodes
+    {
+        private const string Prefix = nameof(Core);
+
+        /// <summary>
+        /// Validation failed.
+        /// </summary>
+        public static ErrorCodeInfo ValidationFailed => new ErrorCodeInfo(Prefix, nameof(ValidationFailed), "Validation failed.");
+
+        /// <summary>
+        /// Unhandled error.
+        /// </summary>
+        public static ErrorCodeInfo UnhandledError => new ErrorCodeInfo(Prefix, nameof(UnhandledError), "Unhandled error.");
+
+        /// <summary>
+        /// Resource unavailable.
+        /// </summary>
+        public static ErrorCodeInfo ResourceUnavailable => new ErrorCodeInfo(Prefix, nameof(ResourceUnavailable), "Resource unavailable.");
+
+        /// <summary>
+        /// Connection failed.
+        /// </summary>
+        public static ErrorCodeInfo ConnectionFailed => new ErrorCodeInfo(Prefix, nameof(ConnectionFailed), "Connection failed.");
+
+        /// <summary>
+        /// Version conflict.
+        /// </summary>
+        public static ErrorCodeInfo VersionConflict => new ErrorCodeInfo(Prefix, nameof(VersionConflict), "Version conflict.");
+
+        /// <summary>
+        /// Access denied.
+        /// </summary>
+        public static ErrorCodeInfo AccessDenied => new ErrorCodeInfo(Prefix, nameof(AccessDenied), "Access denied.");
+
+        /// <summary>
+        /// Agregate root hasn't been found.
+        /// </summary>
+        public static ErrorCodeInfo AggregateRootNotFound => new ErrorCodeInfo(Prefix, nameof(AggregateRootNotFound), "Agregate Root hasn't been found.");
+    }
+}
---
+++ src/Intellias.CQRS.Core/Results/Errors/ErrorCodeInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Intellias.CQRS.Core.Results.Errors
+{
+    /// <summary>
+    /// Info about Error Code.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public class ErrorCodeInfo
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ErrorCodeInfo"/> class.
+        /// </summary>
+        /// <param name="subDomain">SubDomain as prefix.</param>
+        /// <param name="internalCode">InternalCode.</param>
+        /// <param name="message">Message.</param>
+        public ErrorCodeInfo(string subDomain, string internalCode, string message)
+        {
+            Code = $"{subDomain}.{internalCode}";
+            Message = message;
+        }
+
+        /// <summary>
+        /// Code.
+        /// </summary>
+        public string Code { get; protected set; }
+
+        /// <summary>
+        /// Message.
+        /// </summary>
+        public string Message { get; protected set; }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return Message;
+        }
+    }
+}
