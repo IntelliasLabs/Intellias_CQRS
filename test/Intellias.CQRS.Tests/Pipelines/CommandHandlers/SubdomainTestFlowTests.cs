eb22db39f02b2854dc9e4524716b749ed0568b0a
Pavlo Rytikov <prytikov@intellias.com>
2019-12-23 15:07:14 +0200

    Add tests

--- test/Intellias.CQRS.Tests/Pipelines/CommandHandlers/SubdomainTestFlowTests.cs
+++ test/Intellias.CQRS.Tests/Pipelines/CommandHandlers/SubdomainTestFlowTests.cs
@@ -79,7 +79,43 @@ namespace Intellias.CQRS.Tests.Pipelines.CommandHandlers
         }
 
         [Fact]
-        public async Task SubdomainTestFlow_StateBasedFlow_UpdatesState()
+        public async Task SubdomainTestFlowWithState_ExecutionSucceed_UpdatesContext()
+        {
+            mediator.SetupRequestHandler<CommandRequest<FakeCommand>, IExecutionResult>(request => new SuccessfulResult());
+
+            var step = new FakeCommandStep();
+
+            var (result, context) = await testHost.CreateFlow<FakeSubdomainState>()
+                .With(step)
+                .RunAsync();
+
+            result.Should().BeOfType<SuccessfulResult>();
+            context.ExecutedCommands.Single().Should().BeOfType<FakeCommand>()
+                .Which.Key.Should().Be(step.CommandKey);
+            context.ExpectedEvents.Single().Should().BeOfType<FakeIntegrationEvent>()
+                .Which.Key.Should().Be(step.EventKey);
+        }
+
+        [Fact]
+        public async Task SubdomainTestFlowWithState_ExecutionFailed_StopsExecution()
+        {
+            mediator.SetupRequestHandler<CommandRequest<FakeCommand>, IExecutionResult>(request => new FailedResult(CoreErrorCodes.UnhandledError));
+
+            var step1 = new FakeCommandStep();
+            var step2 = new FakeCommandStep();
+
+            var (result, context) = await testHost.CreateFlow<FakeSubdomainState>()
+                .With(step1)
+                .With(step2)
+                .RunAsync();
+
+            result.Should().BeOfType<FailedResult>();
+            context.ExecutedCommands.Should().HaveCount(1);
+            context.ExpectedEvents.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public async Task SubdomainTestFlowWithState_Always_KeepsStateChanges()
         {
             var (_, context) = await testHost.CreateFlow<FakeSubdomainState>()
                 .With(new FakeIncrementStateCounterStep())
@@ -89,21 +125,42 @@ namespace Intellias.CQRS.Tests.Pipelines.CommandHandlers
             context.State.Counter.Should().Be(2);
         }
 
-        private class FakeEmptyStep : ITestFlowStep
+        [Fact]
+        public async Task SubdomainTestFlowWithState_EmptyStepResult_DoesntUpdateContext()
+        {
+            var (_, context) = await testHost.CreateFlow<FakeSubdomainState>()
+                .With(new FakeEmptyStep())
+                .RunAsync();
+
+            context.ExecutedCommands.Should().BeEmpty();
+            context.ExpectedEvents.Should().BeEmpty();
+        }
+
+        private class FakeEmptyStep : ITestFlowStep, ITestFlowStep<FakeSubdomainState>
         {
             public ITestFlowStepResult Execute(TestFlowExecutionContext context)
             {
                 return new EmptyStepResult();
             }
+
+            public ITestFlowStepResult Execute(TestFlowExecutionContext<FakeSubdomainState> context)
+            {
+                return new EmptyStepResult();
+            }
         }
 
-        private class FakeCommandStep : ITestFlowStep
+        private class FakeCommandStep : ITestFlowStep, ITestFlowStep<FakeSubdomainState>
         {
             public string CommandKey { get; } = FixtureUtils.String();
 
             public string EventKey { get; } = FixtureUtils.String();
 
             public ITestFlowStepResult Execute(TestFlowExecutionContext context)
+            {
+                return new CommandStepResult(new FakeCommand(CommandKey), new FakeIntegrationEvent(EventKey));
+            }
+
+            public ITestFlowStepResult Execute(TestFlowExecutionContext<FakeSubdomainState> context)
             {
                 return new CommandStepResult(new FakeCommand(CommandKey), new FakeIntegrationEvent(EventKey));
             }
