96fa04ed7917b88a684dc3e4a869a9aa936804ea
Pavlo Rytikov <prytikov@intellias.com>
2020-02-24 16:12:34 +0200

    Disable R# with the comment

--- src/Intellias.CQRS.Core/Commands/ICommandBus{TCommandBusOptions}.cs
+++ src/Intellias.CQRS.Core/Commands/ICommandBus{TCommandBusOptions}.cs
@@ -6,6 +6,7 @@ namespace Intellias.CQRS.Core.Commands
     /// Publishes commands.
     /// </summary>
     /// <typeparam name="TCommandBusOptions">Command bus options type.</typeparam>
+    // ReSharper disable once UnusedTypeParameter, parameter is used to have multiple instances of Command Bus with the different options.
     public interface ICommandBus<TCommandBusOptions> : IMessageBus<ICommand>
         where TCommandBusOptions : ICommandBusOptions
     {
