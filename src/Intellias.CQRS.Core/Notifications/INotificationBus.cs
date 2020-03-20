c7a047c1852244648f0d534500f5f300bd7def45
Oleh Halay <ohalay@intellias.com>
2020-03-20 16:54:05 +0000

    Merged PR 2266: Add abstraction for notification
    Add abstraction for notification
    
    Related work items: #1982

---
+++ src/Intellias.CQRS.Core/Notifications/INotificationBus.cs
@@ -0,0 +1,11 @@
+﻿using Intellias.CQRS.Core.Messages;
+
+namespace Intellias.CQRS.Core.Notifications
+{
+    /// <summary>
+    /// Abstraction of notification bus.
+    /// </summary>
+    public interface INotificationBus : IMessageBus<Notification>
+    {
+    }
+}
---
+++ src/Intellias.CQRS.Core/Notifications/Notification.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Intellias.CQRS.Core.Messages;
+
+namespace Intellias.CQRS.Core.Notifications
+{
+    /// <summary>
+    /// Base class for notification.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public abstract class Notification : AbstractMessage
+    {
+        /// <summary>
+        /// Notification recipient id.
+        /// </summary>
+        public string RecipientId { get; set; }
+    }
+}
