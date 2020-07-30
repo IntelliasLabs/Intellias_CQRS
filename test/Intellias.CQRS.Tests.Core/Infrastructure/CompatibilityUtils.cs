b12273cdd085bb048cf2c4672c56868886034101
Sergey Seletsky <SeletskySergey@live.com>
2020-07-30 11:21:33 +0300

    fix

--- test/Intellias.CQRS.Tests.Core/Infrastructure/CompatibilityUtils.cs
+++ test/Intellias.CQRS.Tests.Core/Infrastructure/CompatibilityUtils.cs
@@ -133,7 +133,10 @@ namespace Intellias.CQRS.Tests.Core.Infrastructure
             var dirs = Directory.GetDirectories(path).Select(x => Path.GetFullPath(x)).ToList();
             foreach (var dir in dirs)
             {
-                files.AddRange(GetProjectFiles(dir));
+                if (Directory.Exists(dir))
+                {
+                    files.AddRange(GetProjectFiles(dir));
+                }
             }
 
             files.AddRange(dirs.SelectMany(dir => Directory.GetFiles(dir, "*.csproj")).ToList());
